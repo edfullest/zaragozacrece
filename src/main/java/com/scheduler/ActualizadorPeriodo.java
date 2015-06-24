035923d25e529fdcfada6b1ed11097dda915ac4f
Eduardo Jesus Serna Leal <lalotahoma@icloud.com>
2015-06-24 09:58:44 -0500

    Bug fixes.

---
+++ src/main/java/com/scheduler/ActualizadorPeriodo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.scheduler;
+
+import java.util.Date;
+import org.quartz.Job;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.SimpleTrigger;
+
+/**
+ *
+ * @author Lalo Serna
+ */
+public class ActualizadorPeriodo implements Job {
+    
+
+   public void execute(JobExecutionContext context) throws JobExecutionException {
+    // Say Hello to the World and display the date/time
+
+}
+    
+}
